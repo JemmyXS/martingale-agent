0fa7a70e63ed98d2471e2985cc6d926bb56a3860
JemmyXS <xiaoshaogui@foxmail.com>
2025-06-29 14:39:55 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+baseUrl = "https://api.competitions.recall.network/api"
+response = requests.get(
+  f"{baseUrl}/account/portfolio",
+  headers={
+    "Content-Type": "application/json",
+    "Authorization": `Bearer YOUR_API_KEY`,
+  }
+)
+print(response.json())
